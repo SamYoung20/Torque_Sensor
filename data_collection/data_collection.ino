2a5405829426f6dbb24e293607584707a51a6331
Samantha Young <syoung@olin.edu>
2020-03-01 16:41:22 -0500

    changing name of data collector scriot

---
+++ data_collection/data_collection.ino
@@ -0,0 +1,18 @@
+float sensorValue = 0;
+int counter = 0;
+long baudRate = 9600;
+
+void setup(){
+    Serial.begin(baudRate);
+}
+
+void (){
+
+}
+void loop(){
+    Serial.print('\n');
+    Serial.print(counter);
+    Serial.print(sensorValue);
+    counter += 1;
+    delay(500);
+}
